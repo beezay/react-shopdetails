6e16ae284fe1c074439e821aa02ef78203b05a0d
beezay bohora <bijay.okur@gmail.com>
2021-05-05 13:03:21 +0545

    shop delted with storage from single mall page

--- src/components/MallPage/MallsDetails.js
+++ src/components/MallPage/MallsDetails.js
@@ -118,7 +118,7 @@ const MallsDetails = () => {
     let newMall = [...mall];
     newMall[0].shops = [...dbShops, shopData];
     setMall(newMall);
-    dispatch(resetShops());
+    setShopImages([])
   };
   console.log("Mall", mall);
 
@@ -135,22 +135,29 @@ const MallsDetails = () => {
         (img) => img.shopImgId
       );
       console.log(shopImagesName);
-
-      if (shopImagesName.length > 0) {
-        await Promise.all(
-          shopImagesName?.map((img) =>
-            storage
-              .ref("shopImages")
-              .child(img)
-              .delete()
-              .then(() => console.log("Image Deleted"))
-          )
-        );
+      try {
+        if (shopImagesName.length > 0) {
+          await Promise.all(
+            shopImagesName?.map((img) =>
+              storage
+                .ref("shopImages")
+                .child(img)
+                .delete()
+                .then(() => console.log("Image Deleted"))
+            )
+          );
+        }
+        await fireStore
+          .collection("mallInfo")
+          .doc(mallId)
+          .update({ shops: [...remainingShops] })
+          .then(() => {
+            console.log("All task Done");
+            window.location.reload();
+          });
+      } catch (e) {
+        console.log(e);
       }
-      await fireStore
-        .collection("mallInfo")
-        .doc(mallId)
-        .update({ shops: [...remainingShops] });
     }
   };
   return (
